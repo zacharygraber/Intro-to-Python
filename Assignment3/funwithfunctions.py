c316358f43133b73bd08b60f2c44810d1c894950
zegraber <zegraber@iu.edu>
2019-09-20 10:20:21 -0400

    updated funwithfunctions and created CodeDemos

---
+++ Assignment3/funwithfunctions.py
@@ -0,0 +1,45 @@
+import math
+
+#1 
+def y(d,r,t):
+    return d * math.exp(r * t)
+
+#2
+def N(n_0, m, t):
+    return n_0 * math.exp(m * t)
+
+#3
+def N_t(t):
+    return 71.8 * math.exp(-8.96 * math.exp(-0.0685 * t))
+
+#4
+def K(t):
+    return ((9 / 2.6) * t) / (2 * (t ** 2) +3)
+
+#5
+def r(t):
+    return 1.5207 * (t ** 4) - 19.166 * (t ** 3) + 62.91 * (t ** 2) + 6.0726 * t + 1026
+
+#6
+def p(x):
+    return 4 * (x ** 2) - 100 * x - 1000
+
+#7
+def W(P_i,P_f):
+#TO DO: IMPLEMENT FUNCTION
+#8
+def depreciate(c,s,life):
+#TO DO: IMPLEMENT FUNCTION
+#9
+def L(k,V,A,C):
+#TO DO: IMPLEMENT FUNCTION
+if __name__=="__main__":
+    print(y(1000,.02,10))
+    print(N(500,100,4))
+    print(math.floor(N_t(1000)))
+    print(K(1))
+    print(r(4))
+    print(p(10))
+    print(W(10,1))
+    print(depreciate(20000,1000,5))
+    print(L(0.0033,33.8,512,0.515))
---
+++ CodeDemos/fals.py
@@ -0,0 +1,9 @@
+def sumListFor(aList):
+    """
+    Sums all numbers in a list using a for-loop.
+    Returns the sum of all numbers.
+    """
+    result = 0
+    for number in aList:
+        result += number
+    return result
