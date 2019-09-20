2e9343bffbc1481a36ab88597216726b8f2a04af
zegraber <zegraber@iu.edu>
2019-09-20 10:26:00 -0400

    wrote sumListFor and test cases

---
+++ CodeDemos/falsTest.py
@@ -0,0 +1,11 @@
+from fals import *
+
+testList1 = [1,2,3,4,5]
+testList2 = [6,7,8,9,0]
+
+print("Function testing")
+
+print("Testing sumListFor")
+print(sumListFor(testList1))
+print(sumListFor(testList2))
+
