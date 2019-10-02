b4cf3ec0a0ddf2a2f897e3bd7f08bc27332e089d
zegraber <zegraber@iu.edu>
2019-10-02 11:59:23 -0400

    finished makeitrain

---
+++ Assignment4/makeitrain.py
@@ -0,0 +1,21 @@
+def dollars(x: int or float):
+    """
+    Takes in a dollar amount
+    Returns a list [quarters, dimes, nickels, pennies]
+    """
+    q = x // 0.25
+    x = x % 0.25
+
+    d = x // 0.1
+    x = x % 0.1
+
+    n = x // 0.05
+    x = x % 0.05
+
+    p = x / 0.01
+
+    return [q, d, n, float("{:1f}".format(p))]
+
+print(dollars(2.24))
+print(dollars(1.19))
+print(dollars(4.16))
