53f2758174edbe3c476e219ea37a01a6c940e60b
zegraber <zegraber@iu.edu>
2019-10-04 10:39:32 -0400

    created files/structures for lab5

---
+++ Labs/Lab5/lab5.py
@@ -0,0 +1,8 @@
+# Will be stored in `Labs/Lab5/lab5.py`. 
+
+# Tasks for this problem is:
+# - In the python file, write (so overwrite the file each time) a series of non-repeating 
+#   numbers (more than 10 numbers) to a file in the path `Labs/Lab5/storage.txt`
+# - After writing to the file, read from the same file to get a total score
+# - Append the total score to the file
+# - Read from said file again and get the total score.
