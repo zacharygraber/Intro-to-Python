97db050df424ef936a8746551df9ed9fa66c5f41
zegraber <zegraber@iu.edu>
2019-10-04 09:54:35 -0400

    experimented with file reading

--- CodeDemos/Lab5/filing.py
+++ CodeDemos/Lab5/filing.py
@@ -5,3 +5,12 @@ path_of_execution = os.getcwd()
 print("~"*30)
 print("Current location where python program is running: ")
 print("\t" + path_of_execution)
+
+someFile = open("CodeDemos/Lab5/blank.txt", "r")
+
+contents = someFile.read().split(" ")
+
+someFile.close()
+
+for i in contents:
+    print(i)
