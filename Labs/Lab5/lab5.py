6af4cb5f6c82ccd7860481cfa022126ac7d32f84
zegraber <zegraber@iu.edu>
2019-10-04 11:01:30 -0400

    completed steps 1 and 2 in lab5

--- Labs/Lab5/lab5.py
+++ Labs/Lab5/lab5.py
@@ -13,10 +13,25 @@ def randomNumbers(num: "int <= 100"):
         thisNum = random.randint(1,100)
         while str(thisNum) in numberStr:
             thisNum = random.randint(1,100)
-        numberStr += str(thisNum) + " "
+        if i != range(num)[-1]:
+            numberStr += str(thisNum) + " "
+        else:
+            numberStr += str(thisNum)
     return numberStr
+
+def sumScore(scoreString):
+    scoreList = scoreString.split()
+    total = 0
+    for i in scoreList:
+        total += int(i)
+    return total
 
 
 workingFile = open("Labs/Lab5/storage.txt", "w")
 workingFile.write(randomNumbers(50))
 workingFile.close()
+
+workingFile = open("Labs/Lab5/storage.txt", "r")
+totalScore = sumScore(workingFile.read())
+workingFile.close()
+print(totalScore)
