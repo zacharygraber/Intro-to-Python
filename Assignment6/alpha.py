7415e99e137db37a98f2cf407c0df891d5ea2c95
zegraber <zegraber@iu.edu>
2019-10-14 20:53:42 -0400

    added annotations

--- Assignment6/alpha.py
+++ Assignment6/alpha.py
@@ -7,14 +7,16 @@ def alphaFunction():
     RETURN a dict of lowercase letters counted
     """
     d = {}
+
+    #for each lowercase letter, add it to d and assign it 0
     for i in range(97,123):
         d[chr(i)] = 0
     
-    with open("Assignment6/happy.txt", "r") as f:
+    with open("Assignment6/happy.txt", "r") as f: #read the file as f
         contents = f.read()
-        for i in contents:
+        for i in contents: #for each letter in contents
             ordi = ord(i)
-            if ordi > 96 and ordi < 123:
+            if ordi > 96 and ordi < 123: #if it is a lowercase letter
                 d[i] += 1
     
     return d
@@ -22,4 +24,4 @@ def alphaFunction():
 if __name__ == "__main__":
     returnOfTheDict = alphaFunction()
     for i in returnOfTheDict:
-        print(i,returnOfTheDict[i])
+        print(i,returnOfTheDict[i]) #print each letter and its occurrence side by side
