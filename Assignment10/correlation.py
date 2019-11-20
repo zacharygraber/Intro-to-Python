13e9f76707d9489dbab0b1ed95a0b8c03a3ab83e
zegraber <zegraber@iu.edu>
2019-11-20 17:46:49 -0500

    finished correlation

--- Assignment10/correlation.py
+++ Assignment10/correlation.py
@@ -1,27 +1,68 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import sqrt
 
 def mean(lst):
-    # TODO: Implement function
-	return 1
+	return sum(lst) / len(lst)
 
 def sd(xlst):
-    # TODO: Implement function
-    return 1
+    sigma = []
+    xlstMean = mean(xlst)
+    for xi in xlst:
+        sigma += [(xi - xlstMean)**2]
+    return sqrt(( 1 / (len(xlst) - 1)) * sum(sigma))
 
 def r(x, y):
-    # TODO: Implement function
-    return 1
+    #print(sd(x))
+    sigma = []
+    xMean = mean(x)
+    yMean = mean(y)
+    for i in range(len(x)):
+        sigma += [((x[i] - xMean) / (sd(x))) * ((y[i] - yMean) / (sd(y)))]
+    return (1/(len(x)-1)) * sum(sigma)
 
 def theGraph():
-    pass
-    # TODO: Implement function (D2)
+    def leastSquareBestFit(x,y):
+        xMean = mean(x)
+        yMean = mean(y)
+        numSigma = [(x[i] - xMean) * (y[i] - yMean) for i in range(len(x))]
+        denSigma = [(x[i] - xMean)**2 for i in range(len(x))]
+
+        slope = sum(numSigma) / sum(denSigma)
+        intercept = yMean - (slope * xMean)
+        return lambda x: (slope*x) + intercept
+
+    # open the file and generate a list dat with the coordinates listed as space-separated strings
+    with open("Assignment10/acme_zyx.txt") as f:
+        dat = (f.read()).splitlines()
+    
+    # convert each coordinate into a list of strings
+    for i in range(len(dat)):
+        dat[i] = dat[i].split()
+    
+    # generate lists of float x and y values
+    x = [float(i[0]) for i in dat]
+    y = [float(i[1]) for i in dat]
+    abscissa = np.arange(0,125,5)
+    
+    rValue = r(x,y)
+    plt.plot(x,y, "ro")
+    plt.plot(abscissa,leastSquareBestFit(x,y)(abscissa), "g--")
+    
+    plt.axis([0,120,0,210])
+    plt.xlabel("ACME")
+    plt.ylabel("ZYX")
+    plt.title("r = {0:0.3}".format(rValue))
+    plt.savefig(fname="Assignment10/stock.png")
+
+
 
 
 if __name__ == "__main__":
     x = [[2,3],[4,3],[1,1.4],[1,.5],[5,3]]
 
-    # TODO: Complete code (D1) by calling the important function
+    # Complete code (D1) by calling the important function
+    rValue = r([i[0] for i in x],[i[1] for i in x])
 
     print(rValue)
     # Example of creating a plot
