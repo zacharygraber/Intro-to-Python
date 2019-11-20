f106c780694ae704aeddd8ed133b9a9e1a4a5412
zegraber <zegraber@iu.edu>
2019-11-20 11:55:17 -0500

    finished matrix

--- Assignment10/matrix.py
+++ Assignment10/matrix.py
@@ -3,28 +3,43 @@ import numpy as np
 #INPUT two matrices a,b
 #RETURN product ab
 def mm(a,b):
-    # TODO: Implement function
-    return 1
+    ans = np.zeros((a.shape[0],b.shape[1]))
+    for row in range(ans.shape[0]):
+        for column in range(ans.shape[1]):
+            dp = []
+            for i in range(a.shape[1]):
+                dp += [a[row][i] * b[i][column]]
+            ans[row][column] = sum(dp)
+    return ans
 
 
 #INPUT scalar n and matrix a
 #RETURN scalar product na
 def sm(n,a):
-    # TODO:Implement function
-    return 1
+    ans = np.zeros(a.shape)
+    for row in range(a.shape[0]):
+        for column in range(a.shape[1]):
+            ans[row][column] = a[row][column] * n
+    return ans
 
 
 #INPUT matrix n x m
 #RETURN transpose matrix m x n
 def tp(a):
-    # TODO:Implement function
-	return 1
+    ans = np.zeros((a.shape[1],a.shape[0]))
+    for i in range(0,a.shape[0]):
+        for j in range(0,a.shape[1]):
+            ans[j][i] = a[i][j]
+    return ans
 
 #INPUT two matrices a,b
 #RETURN sum a + b
 def add_m(a,b):
-    # TODO: Implement function
-	return 1
+    s = np.zeros(a.shape)
+    for row in range(0,a.shape[0]):
+        for column in range(0,a.shape[1]):
+            s[row][column] = a[row][column] + b[row][column]
+    return s
 
 if __name__ == "__main__":
     a = np.array([[1,2,4],[3,4,3]])
