8acf0c48e3fce22a955867c2ddc15940baff981a
zegraber <zegraber@iu.edu>
2019-12-13 22:58:06 -0500

    attempted codd

--- Assignment11/codd.py
+++ Assignment11/codd.py
@@ -2,9 +2,7 @@ import sqlite3
 
 dog = sqlite3.connect("example1.db")
 c = dog.cursor()
-r = [('c','a'),('a','b'), ('a','e'),('c','d'),
-          ('c','f'),('f','g'), ('f','h'),('h','j'),
-          ('h','i')]
+r = [('c','a'),('a','b'), ('a','e'),('c','d'),('c','f'),('f','g'), ('f','h'),('h','j'),('h','i')]
 
 c.execute('DROP TABLE IF EXISTS G')
 c.execute('''CREATE TABLE G (Parent text, Child)''')
@@ -14,7 +12,7 @@ c.executemany('INSERT INTO G VALUES (?,?)', r)
 dog.commit()
 
 
-q = #TO DO: Implement Lambda function
+q = lambda x,y: f"SELECT Parent text FROM G WHERE (SELECT Parent text FROM G WHERE Child = '{x}') = (SELECT Parent text FROM G WHERE Child = '{y}')"
 
 print(c.execute(q('b','e')).fetchone())
 print(c.execute(q('h','g')).fetchone())
